cf5be43d55c02e984edadd95fa9e8b4702e57520
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2020-09-29 08:37:38 -0700

    - (Check-in) Updated the attendance code service to avoid duplicate codes in a Redis Backplane environment.

--- Rock/Model/AttendanceCodeService.Partial.cs
+++ Rock/Model/AttendanceCodeService.Partial.cs
@@ -32,6 +32,13 @@ namespace Rock.Model
         private static DateTime? _todaysDate = null;
         private static HashSet<string> _todaysUsedCodes = null;
 
+        // We're only going to attempt to get a code 1 million times...
+        // Interestingly, even when this code approaches the maximum number of possible combinations
+        // it still typically takes less than 5000 attempts. However, if the number of
+        // attempts jumps over 10,000 there is almost certainly a problem with someone's
+        // check-in code configuration so we're going to stop after a million attempts.
+        private static readonly int _MaxAttempts = 1000000;
+
         /// <summary>
         /// An array of characters that can be used as a part of  <see cref="Rock.Model.AttendanceCode">AttendanceCodes</see>
         /// </summary>
@@ -50,12 +57,26 @@ namespace Rock.Model
         /// <summary>
         /// A list of <see cref="System.String"/> values that are not allowable as attendance codes.
         /// </summary>
-        public static readonly List<string> _NoGood = new List<string> {
+        public static readonly List<string> noGood = new List<string> {
             "4NL", "4SS", "5CK", "5HT", "5LT", "5NM", "5TD", "5XX", "666", "BCH", "CLT", "CNT", "D4M", "D5H", "DCK", "DMN", "DSH", "F4G", "FCK", "FGT", "G4Y", "GZZ", "H8R",
             "JNK", "JZZ", "KKK", "KLT", "KNT", "L5D", "LCK", "LSD", "MFF", "MLF", "ND5", "NDS", "NDZ", "NGR", "P55", "PCP", "PHC", "PHK", "PHQ", "PM5", "PMS", "PN5", "PNS",
             "PRC", "PRK", "PRN", "PRQ", "PSS", "RCK", "SCK", "S3X", "SHT", "SLT", "SNM", "STD", "SXX", "THC", "V4G", "WCK", "XTC", "XXX", "911", "1XL", "2XL", "3XL", "4XL",
             "5XL", "6XL", "7XL", "8XL", "9XL", "XXL"
         };
+
+        /// <summary>
+        /// Determines whether the provided code is already in use for today by querying the DB.
+        /// </summary>
+        /// <param name="code">The code.</param>
+        /// <returns>
+        ///   <c>true</c> if [is code already in use] [the specified code]; otherwise, <c>false</c>.
+        /// </returns>
+        public bool IsCodeAlreadyInUse( string code )
+        {
+            DateTime today = RockDateTime.Today;
+            DateTime tomorrow = today.AddDays( 1 );
+            return this.Queryable().AsNoTracking().Where( c => c.IssueDateTime >= today && c.IssueDateTime < tomorrow && c.Code == code ).Any();
+        }
 
         /// <summary>
         /// Returns a new <see cref="Rock.Model.AttendanceCode"/> comprised of random alpha numeric characters.
@@ -93,7 +114,7 @@ namespace Rock.Model
         /// <param name="numericLength">Length of the numeric.</param>
         /// <param name="isRandomized">if set to <c>true</c> [is randomized].</param>
         /// <returns></returns>
-        public static AttendanceCode GetNew( int alphaNumericLength, int alphaLength, int numericLength, bool isRandomized )
+        public static AttendanceCode GetNewOld( int alphaNumericLength, int alphaLength, int numericLength, bool isRandomized )
         {
             lock ( _Obj )
             {
@@ -120,7 +141,7 @@ namespace Rock.Model
                         alphaNumericCode =
                             ( alphaNumericLength > 0 ? GenerateRandomCode( alphaNumericLength ) : string.Empty ) +
                             ( alphaLength > 0 ? GenerateRandomAlphaCode( alphaLength ) : string.Empty );
-                        while ( _NoGood.Any( s => alphaNumericCode.Contains( s ) ) || _todaysUsedCodes.Contains( alphaNumericCode ) )
+                        while ( noGood.Any( s => alphaNumericCode.Contains( s ) ) || _todaysUsedCodes.Contains( alphaNumericCode ) )
                         {
                             attempts++;
 
@@ -163,6 +184,103 @@ namespace Rock.Model
         }
 
         /// <summary>
+        /// Gets the new.
+        /// </summary>
+        /// <param name="alphaNumericLength">Length of the alpha numeric.</param>
+        /// <param name="alphaLength">Length of the alpha.</param>
+        /// <param name="numericLength">Length of the numeric.</param>
+        /// <param name="isRandomized">if set to <c>true</c> [is randomized].</param>
+        /// <returns></returns>
+        public static AttendanceCode GetNew( int alphaNumericLength, int alphaLength, int numericLength, bool isRandomized )
+        {
+            lock ( _Obj )
+            {
+                using ( var rockContext = new Rock.Data.RockContext() )
+                {
+                    var service = new AttendanceCodeService( rockContext );
+
+                    DateTime today = RockDateTime.Today;
+                    if ( _todaysUsedCodes == null || !_todaysDate.HasValue || !_todaysDate.Value.Equals( today ) )
+                    {
+                        _todaysDate = today;
+                        DateTime tomorrow = today.AddDays( 1 );
+
+                        _todaysUsedCodes = new HashSet<string>( service.Queryable().AsNoTracking().Where( c => c.IssueDateTime >= today && c.IssueDateTime < tomorrow ).Select( c => c.Code ).ToList() );
+                    }
+
+                    string alphaNumericCode = string.Empty;
+                    string alphaCode = string.Empty;
+                    string numericCode = string.Empty;
+                    string code = string.Empty;
+
+                    for ( int attempts = 0; attempts <= _MaxAttempts; attempts++ )
+                    {
+                        if ( attempts == _MaxAttempts )
+                        {
+                            throw new TimeoutException( "Too many attempts to create a unique attendance code.  There is almost certainly a check-in system 'Security Code Length' configuration problem." );
+                        }
+
+                        if ( alphaNumericLength > 0 )
+                        {
+                            alphaNumericCode = GenerateRandomCode( alphaNumericLength );
+                        }
+
+                        if ( alphaLength > 0 )
+                        {
+                            alphaCode = GenerateRandomAlphaCode( alphaLength );
+                        }
+
+                        if ( numericLength > 0 )
+                        {
+                            int codeLen = alphaNumericLength + alphaLength + numericLength;
+                            var lastCode = _todaysUsedCodes.Where( c => c.Length == codeLen ).OrderBy( c => c.Substring( alphaNumericLength + alphaLength ) ).LastOrDefault();
+                            numericCode = GetNextNumericCodeAsString( alphaNumericLength, alphaLength, numericLength, isRandomized, lastCode );// Need to change last code if this doesn't return a good number
+                        }
+
+                        code = alphaNumericCode + alphaCode + numericCode;
+
+                        // Check if code is already in use or contains bad unallowed strings.
+                        if ( noGood.Any( s => code.Contains( s ) ) || _todaysUsedCodes.Contains( code ) )
+                        {
+                            alphaNumericCode = string.Empty;
+                            alphaCode = string.Empty;
+                            numericCode = string.Empty;
+                            code = string.Empty;
+                            continue;
+                        }
+
+                        // When using a clustered environment we need to check the DB to make sure the code hasn't been assigned by another server
+                        if ( Rock.Utility.Settings.RockInstanceConfig.IsClustered )
+                        {
+                            if ( service.IsCodeAlreadyInUse( code ) )
+                            {
+                                alphaNumericCode = string.Empty;
+                                alphaCode = string.Empty;
+                                numericCode = string.Empty;
+                                code = string.Empty;
+                                continue;
+                            }
+                        }
+
+                        // If we get to this point the code can be used
+                        break;
+                    }
+
+                    _todaysUsedCodes.Add( code );
+
+                    var attendanceCode = new AttendanceCode();
+                    attendanceCode.IssueDateTime = RockDateTime.Now;
+                    attendanceCode.Code = code;
+
+                    service.Add( attendanceCode );
+                    rockContext.SaveChanges();
+
+                    return attendanceCode;
+                }
+            }
+        }
+
+        /// <summary>
         /// Gets the next numeric code as string.
         /// </summary>
         /// <param name="alphaNumericLength">Length of the alpha numeric.</param>
@@ -182,7 +300,7 @@ namespace Rock.Model
                 numericCode = GenerateRandomNumericCode( numericLength );
 
                 // #2877, use contains to prevent leading zeros bypassing a match for 666
-                while ( _NoGood.Any( s => numericCode.Contains( s ) ) || _todaysUsedCodes.Any( c => c.EndsWith( numericCode ) ) )
+                while ( noGood.Any( s => numericCode.Contains( s ) ) || _todaysUsedCodes.Any( c => c.EndsWith( numericCode ) ) )
                 {
                     attempts++;
 
